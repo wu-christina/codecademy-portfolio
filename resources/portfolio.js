8182e5495ebff3405209bf1ea4b94f44d9578290
Christina Wu <wu_christina@outlook.com>
2022-05-04 14:22:34 -0400

    Commit

---
+++ resources/portfolio.js
@@ -0,0 +1,12 @@
+function get_uptime() {
+    var t1 = new Date(2022, 04, 01, 0, 0, 0, 0)
+    var t2 = new Date()
+    var dif = t1.getTime() - t2.getTime()
+
+    seconds = dif / 100;
+    Seconds_Between_Dates = Math.round(Math.abs(seconds));
+    document.getElementById("seconds").innerHTML = Seconds_Between_Dates;
+    setTimeout(get_uptime, 1000);
+}
+
+get_uptime();
