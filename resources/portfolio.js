ca97d735fcc9923b30e1966b408007e493ff6983
wu-christina <102542554+wu-christina@users.noreply.github.com>
2022-05-04 14:53:14 -0400

    Add files via upload
    Change date format to "2022-04-01"

--- resources/portfolio.js
+++ resources/portfolio.js
@@ -1,12 +1,12 @@
 function get_uptime() {
-    var t1 = new Date(2022, 04, 01, 0, 0, 0, 0)
+    var t1 = new Date("2022-04-01")
     var t2 = new Date()
     var dif = t1.getTime() - t2.getTime()
 
     seconds = dif / 1000;
     Seconds_Between_Dates = Math.round(Math.abs(seconds));
     document.getElementById("seconds").innerHTML = Seconds_Between_Dates;
-    setTimeout(get_uptime, 1000);
+    setTimeout(get_uptime);
 }
 
 get_uptime();
