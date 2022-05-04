4ca090e2cb0a4ea9997a3892dc4015832dc7a3aa
wu-christina <102542554+wu-christina@users.noreply.github.com>
2022-05-04 14:35:25 -0400

    Add files via upload
    Fix formula to calculate seconds

--- resources/portfolio.js
+++ resources/portfolio.js
@@ -3,7 +3,7 @@ function get_uptime() {
     var t2 = new Date()
     var dif = t1.getTime() - t2.getTime()
 
-    seconds = dif / 100;
+    seconds = dif / 1000;
     Seconds_Between_Dates = Math.round(Math.abs(seconds));
     document.getElementById("seconds").innerHTML = Seconds_Between_Dates;
     setTimeout(get_uptime, 1000);
